d91e07f0356b03076e3b2f8a501eeb92a7a34cb6
Andrei <kryvetskistud@gmail.com>
2021-02-03 18:44:27 +0300

    feat: add short versions of code

--- src/index.js
+++ src/index.js
@@ -1,34 +1,46 @@
 
 exports.min = function min (array) {
-  if (array === undefined) return 0
-  if (array.length === 0) return 0
-  let minValue = array[0]
-    for (let i = 0; i < array.length; i++) {
-    if (array[i] < minValue) {
-      minValue = array[i]
-    } 
-  }
-  return minValue
+  //short version
+  if (array === undefined || array.length === 0) return 0
+  return Math.min.apply(null, array)
+
+  // if (array === undefined) return 0
+  // if (array.length === 0) return 0
+  // let minValue = array[0]
+  //   for (let i = 0; i < array.length; i++) {
+  //   if (array[i] < minValue) {
+  //     minValue = array[i]
+  //   } 
+  // }
+  // return minValue
 }
 
 exports.max = function max (array) {
-  if (array === undefined) return 0
-  if (array.length === 0) return 0
-  let maxValue = array[0]
-    for (let i = 0; i < array.length; i++) {
-    if (array[i] > maxValue) {
-      maxValue = array[i]
-    } 
-  }
-  return maxValue
+  //short version
+  if (array === undefined || array.length === 0) return 0
+  return Math.max.apply(null, array)
+
+  // if (array === undefined) return 0
+  // if (array.length === 0) return 0
+  // let maxValue = array[0]
+  //   for (let i = 0; i < array.length; i++) {
+  //   if (array[i] > maxValue) {
+  //     maxValue = array[i]
+  //   } 
+  // }
+  // return maxValue
 }
 
 exports.avg = function avg (array) {
-  if (array === undefined) return 0
-  if (array.length === 0) return 0
-  let sum = 0
-  for (let i = 0; i < array.length; i++) {
-    sum += array[i]
-  } 
-  return (sum / array.length)
+  //short version
+  if (array === undefined || array.length === 0) return 0
+  return (array.reduce(function(acc, value) { return acc + value; }, 0) / array.length)
+
+  // if (array === undefined) return 0
+  // if (array.length === 0) return 0
+  // let sum = 0
+  // for (let i = 0; i < array.length; i++) {
+  //   sum += array[i]
+  // } 
+  // return (sum / array.length)
 }
